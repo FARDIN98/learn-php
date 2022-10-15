b0341b6a8bb6fd87058b2f6bb6fcd4b1a888fc12
FARDIN98 <fardinahmed66@gmail.com>
2022-10-15 23:27:24 +0600

    first commit

---
+++ 2.variable.php
@@ -0,0 +1,11 @@
+<?php 
+
+    $name = 'Hello World';
+    $city = 'Dhaka';
+    echo $name.'</br>';
+    echo $city.'<br/>';
+    echo "<h1>My name is $name and i live in $city</h1>";
+    // concatination
+    echo "<p>My name is</p>" .$name. "<p>and i live in </p>".$city;
+
+?>
---
+++ 3.comments.php
@@ -0,0 +1,11 @@
+<?php 
+
+    $name = 'Hello World';
+    $city = 'Dhaka';
+    echo $name.'</br>';
+    echo $city.'<br/>';
+    echo "<h1>My name is $name and i live in $city</h1>";
+    // concatination
+    echo "<h1>My name is" .$name. "and i live in".$city."</h1>";
+
+?>
